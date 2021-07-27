ba2d0d2ac5d7aa03c93d2dc54920bb6d4cf4cb5d
Shandira <shandira.ferguson@gmail.com>
2021-07-26 23:05:52 -0700

    Banner built

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,15 @@
-const test = () => {
-  return 'Hello World';
-};
+import React from 'react';
 
+class Overview extends React.Component {
+  render(){
+    return(
+      <div>
+        <section>
+          <div className='builder-banner-container'><span className='banner-headline'>Introducing our cleanest denim collection yet.&nbsp; <u>Shop Now</u></span></div>
+        </section>
+      </div>
+    );
+  }
+}
 
-export default test;
+export default Overview;
