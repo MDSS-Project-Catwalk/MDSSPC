06d592188bb64cc8b2690095c05bcf8ebda0e176
Mohamed Zein <mzeinhu@gmail.com>
2021-08-02 09:26:20 -0700

    Added 'See More Questions Button'

--- client/src/components/QA.jsx
+++ client/src/components/QA.jsx
@@ -6,26 +6,53 @@ export const DataContext = React.createContext();
 
 const questionAnswers = (props) => {
   const [data, setData] = useState({ product_id: null, results: [] });
+  const [tempData, setTempData] = useState({ product_id: null, results: [] });
+  const [count, setCount] = useState(4);
+  let seeMoreAnswers;
 
   useEffect(() => {
     axios.get('qa/questions/', {
       params: {
         product_id: 25165,
-        count: 4,
+        count: count,
       },
     })
       .then((result) => {
         setData(currentState => (result.data));
       });
-  }, []);
 
-  return (
-    <div className="QA_list">
-      <DataContext.Provider value={data}>
-        <QuestionsList />
-      </DataContext.Provider>
-    </div>
-  );
+    axios.get('qa/questions/', {
+      params: {
+        product_id: 25165,
+        count: count + 2,
+      },
+    })
+      .then((result) => {
+        setTempData(currentState => (result.data));
+      });
+  }, [count]);
+
+  const moreAnswersHandler = () => {
+    setCount((currentState) => (currentState + 2));
+  };
+
+  if (data.results.length < tempData.results.length) {
+    seeMoreAnswers = <button type="button" onClick={moreAnswersHandler}> More Answered Questions </button>
+  }
+
+  if (tempData.results.length !== 0 && data.results.length !== 0) {
+    return (
+      <div className="QA_list">
+        <DataContext.Provider value={data}>
+          <QuestionsList />
+        </DataContext.Provider>
+        <div className="question-footer">
+          {seeMoreAnswers}
+        </div>
+      </div>
+    );
+  }
+  return null;
 };
 
 export default questionAnswers;
