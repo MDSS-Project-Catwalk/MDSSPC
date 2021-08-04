b66cbe22b2da6dc1a639d3bf6db5c2ec2a6eb79c
Mohamed Zein <mzeinhu@gmail.com>
2021-08-04 14:51:32 -0700

    components in different Files

--- client/src/components/QA/AnswerImageUploadHandler.jsx
+++ client/src/components/QA/AnswerImageUploadHandler.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import useStorage from './hooks/useStorage';
+import useStorage from '../hooks/useStorage';
 
 const AnswerImageUploadHandler = ({files, extract, imageUrlData}) => {
   if (files !== undefined) {
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import QA from './components/QA';
+import QA from './components/QA/QA';
 import OV from './components/overview/OV';
 
 ReactDOM.render(<OV />, document.getElementById('ov'));
