3778428440eca390a9cbaf73fcb1c76a16b53dc0
Mohamed Zein <mzeinhu@gmail.com>
2021-08-04 14:53:22 -0700

    code ready for merge

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import QA from './components/QA/QA';
-import OV from './components/overview/OV';
+import OV from './components/OV/OV';
 
 ReactDOM.render(<OV />, document.getElementById('ov'));
 ReactDOM.render(<QA />, document.getElementById('app'));
