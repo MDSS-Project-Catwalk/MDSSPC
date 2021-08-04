2c56c409a8f0ef8c3968c1bc97e45fd42ace3607
Diego Tobar <datobar89@gmail.com>
2021-08-04 14:53:23 -0700

    About to merge

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import App from './RelatedItems/App.jsx';
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
