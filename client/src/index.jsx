ee31aa4c0c82424e07a85764c3649f8c9cea4eed
soohyung <soohyungl@gmail.com>
2021-08-07 11:37:40 -0700

    ok last time i promise

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,7 +7,7 @@ import "core-js/stable";
 import "regenerator-runtime/runtime";
 import Reviews from './components/RatingsReviews/ReviewApp.jsx';
 
-// ReactDOM.render(<OV />, document.getElementById('ov'));
-// ReactDOM.render(<RelatedItemsComponent />, document.getElementById('ri'));
-// ReactDOM.render(<QA />, document.getElementById('qa'));
+ReactDOM.render(<OV />, document.getElementById('ov'));
+ReactDOM.render(<RelatedItemsComponent />, document.getElementById('ri'));
+ReactDOM.render(<QA />, document.getElementById('qa'));
 ReactDOM.render(<Reviews />, document.getElementById('re'));
