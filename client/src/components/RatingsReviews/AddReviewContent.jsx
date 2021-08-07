426e90b673f27bfb16416aedfeeb2c5fe7744802
soohyung <soohyungl@gmail.com>
2021-08-07 14:20:28 -0700

    changes

--- client/src/components/RatingsReviews/AddReviewContent.jsx
+++ client/src/components/RatingsReviews/AddReviewContent.jsx
@@ -8,7 +8,12 @@ import styled from 'styled-components';
     const FormTwo = styled.form`
     vertical-align: text-top;
     `;
-
+    const Box = styled.div`
+    flex: 1;
+    `;
+    const BoxTwo = styled.div`
+    flex: 3;
+    `;
     const SummaryStyle = styled.textarea`
     width: 250px;
     height: 30px;
@@ -22,20 +27,31 @@ import styled from 'styled-components';
     padding-left: 10px;
     padding-top: 5px;
     `; 
+    const Container = styled.div`
+    display: flex;
+    `;
+    
 const AddReviewContent = () => {
     const [ summary, setSummary ] = useState('');
     const [ body, setBody ] = useState('');
 
 return (
-    <div>
-        <Form onSubmit={()=> {alert('summary submitted')}}>
-            <SummaryStyle type="text" value={summary} onChange={(event)=> {setSummary(event.target.value)}}/>
-        </Form >
-        <FormTwo >
-            <BodyStyle type="text" value={body} onChange={(event)=>{setBody(event.target.value)}}/>
-        </FormTwo>
-        <button className="rr-submit btn"> Submit Review</button>
-    </div>
+    <Container>
+        <Box> Title </Box> &nbsp;&nbsp;
+        <BoxTwo>
+            <Form onSubmit={()=> {alert('summary submitted')}}>
+                <SummaryStyle type="text" value={summary} onChange={(event)=> {setSummary(event.target.value)}}/>
+            </Form >
+        </BoxTwo>&nbsp;&nbsp;
+        <Box> Review 
+        </Box>  &nbsp;&nbsp;
+        <BoxTwo>
+            <FormTwo >
+                <BodyStyle type="text" value={body} onChange={(event)=>{setBody(event.target.value)}}/>
+            </FormTwo>
+            <button className="rr-submit btn"> Submit Review</button>
+        </BoxTwo>
+    </Container>
 )
 }
 
